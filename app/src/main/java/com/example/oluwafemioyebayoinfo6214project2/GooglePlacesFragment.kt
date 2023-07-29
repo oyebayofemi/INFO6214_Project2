e65d6bbd1a9c8078229dfcf77f23d83b063e9c18
oyebayofemi <oyebayooluwafemi@gmail.com>
2023-07-29 06:52:30 -0400

    Creating Menu Options Fragments

---
+++ app/src/main/java/com/example/oluwafemioyebayoinfo6214project2/GooglePlacesFragment.kt
@@ -0,0 +1,25 @@
+package com.example.oluwafemioyebayoinfo6214project2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class GooglePlacesFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_google_places, container, false)
+    }
+
+    }
--- app/src/main/java/com/example/oluwafemioyebayoinfo6214project2/MainActivity.kt
+++ app/src/main/java/com/example/oluwafemioyebayoinfo6214project2/MainActivity.kt
@@ -4,12 +4,50 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.PopupMenu
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val googleMapsFragment =   GoogleMapsFragment()
+        val googlePlacesFragment = GooglePlacesFragment()
+        val emailFragment = EmailFragment()
+        val aboutFragment = AboutFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.fragmentContainer, googleMapsFragment);
+            commit();
+        }
+
         val menuBtn = findViewById<ImageButton>(R.id.menuButton)
+        val menuOptions = PopupMenu(this, menuBtn)
+
+        menuOptions.menuInflater.inflate(R.menu.menu_options, menuOptions.menu)
+        menuOptions.setOnMenuItemClickListener { meniItems ->
+            when(meniItems.itemId)
+            {
+                R.id.google_map_option -> changeFragment(googleMapsFragment)
+                R.id.google_places_option -> changeFragment(googlePlacesFragment)
+                R.id.email_option -> changeFragment(emailFragment)
+                R.id.about_option -> changeFragment(aboutFragment)
+
+            }
+            true
+        }
+
+        menuBtn.setOnClickListener {
+            menuOptions.show()
+        }
+
+    }
+
+    private fun changeFragment(frgamentName: Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainer, frgamentName);
+            commit();
+        }
     }
 }
